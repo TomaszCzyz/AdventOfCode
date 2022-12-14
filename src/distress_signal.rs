49429ac42aaf828b9ed159b2b50fc132eb6e6afa
tczyz <tomczyz97@gmail.com>
2022-12-14 16:39:50 +0100

    WIP

--- src/distress_signal.rs
+++ src/distress_signal.rs
@@ -1,17 +1,28 @@
 #![allow(dead_code)]
 
 use std::borrow::Cow;
+use std::cmp::Ordering;
+use std::collections::VecDeque;
 use std::fmt::Debug;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, Write};
 
+use itertools::PeekingNext;
 use ptree::{print_tree, Style, TreeItem};
+use uuid::Uuid;
 
 #[derive(Clone, Debug)]
 struct Node {
+    id: Uuid,
     value: Option<u32>,
     children: Vec<Node>,
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id && self.value == other.value && self.children == other.children
+    }
 }
 
 impl TreeItem for Node {
@@ -19,7 +30,7 @@ impl TreeItem for Node {
 
     fn write_self<W: Write>(&self, f: &mut W, style: &Style) -> io::Result<()> {
         let text = match self.value {
-            None => "empty".to_string(),
+            None => "E".to_string(),
             Some(val) => val.to_string()
         };
         write!(f, "{}", style.paint(text))
@@ -110,6 +121,7 @@ fn my_split(line: &str) -> Vec<&str> {
 
 fn parse_tree(line: &str) -> Node {
     let mut parent = Node {
+        id: Uuid::new_v4(),
         value: None,
         children: vec![],
     };
@@ -122,13 +134,107 @@ fn parse_tree(line: &str) -> Node {
             parent.children.push(node);
         } else if !part.is_empty() {
             let num = part.parse::<u32>().unwrap();
-            let node = Node { value: Option::from(num), children: vec![] };
+            let node = Node { id: Uuid::new_v4(), value: Option::from(num), children: vec![] };
 
             parent.children.push(node);
         }
     }
 
     parent
+}
+
+fn dfs(root: &Node) -> Vec<i32> {
+    let mut ordered = Vec::new();
+    let mut stack = VecDeque::new();
+    let mut visited = Vec::new();
+
+    stack.push_front(root);
+
+    while !stack.is_empty() {
+        let current_node = stack.pop_front().unwrap();
+
+        if !visited.contains(&current_node) {
+            if let Some(val) = current_node.value {
+                print!("{:?} ", val);
+                ordered.push(val as i32);
+            } else {
+                print!("E ");
+                ordered.push(-1);
+            }
+
+            visited.push(current_node);
+
+            for child in current_node.children.iter().rev() {
+                stack.push_front(child);
+            }
+        }
+    }
+    println!();
+
+    ordered
+}
+
+struct Answer {
+    is_in_order: bool,
+    reason: String,
+}
+
+fn double_dfs_recursion(
+    current_l: &Node,
+    current_r: &Node,
+    visited_l: &mut Vec<Uuid>,
+    visited_r: &mut Vec<Uuid>,
+) -> Option<Answer> {
+    if visited_l.contains(&current_l.id) || visited_r.contains(&current_r.id) { // ?
+        return None;
+    }
+    visited_l.push(current_l.id);
+    visited_r.push(current_r.id);
+
+    if let Some(val_l) = current_l.value {
+        if let Some(val_r) = current_r.value {
+            match val_l.cmp(&val_r) {
+                Ordering::Equal => {
+                    println!("left({:?}) == right({:?}), do nothing...", val_l, val_r);
+                }
+                Ordering::Less => {
+                    let string = format!("CORRECT - because left({}) <= right({})", val_l, val_r);
+                    println!("{}", string);
+                    return Some(Answer { is_in_order: true, reason: string });
+                }
+                Ordering::Greater => {
+                    let string = format!("INCORRECT - because left({}) > right({})", val_l, val_r);
+                    println!("{}", string);
+                    return Some(Answer { is_in_order: false, reason: string });
+                }
+            }
+        }
+    }
+
+    let mut iter_l = current_l.children.iter();
+    let mut iter_r = current_r.children.iter();
+    loop {
+        if let Some(child_l) = iter_l.next() {
+            if let Some(child_r) = iter_r.next() {
+                let answer = double_dfs_recursion(child_l, child_r, visited_l, visited_r);
+                if answer.is_some() {
+                    return answer;
+                };
+            } else {
+                let string = "INCORRECT - Right side ran out of items, so inputs are not in the right order".to_string();
+                println!("{}", string);
+                return Some(Answer { is_in_order: false, reason: string) });
+            }
+        } else if let Some(_next_node) = iter_r.peeking_next(|_| true) {
+            let string = "CORRECT - Left side ran out of items, so inputs are in the right order".to_string();
+            println!("{}", string);
+            return Some(Answer { is_in_order: true, reason: string });
+        } else {
+            // println!("same number of children");
+            break;
+        }
+    }
+    None
 }
 
 pub fn distress_signal_part_1(file_name: &str) -> usize {
@@ -138,7 +244,11 @@ pub fn distress_signal_part_1(file_name: &str) -> usize {
     for (node1, node2) in input {
         let _ = print_tree(&node1);
         let _ = print_tree(&node2);
-        println!();
+
+        let mut visited_l = Vec::new();
+        let mut visited_r = Vec::new();
+
+        double_dfs_recursion(&node1, &node2, &mut visited_l, &mut visited_r);
     }
 
     indices_sum
