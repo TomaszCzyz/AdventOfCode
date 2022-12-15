f95b58657fa01c3491c9fb70ac85987d127c7c3e
tczyz <tomczyz97@gmail.com>
2022-12-15 21:52:16 +0100

    add solution for task 13 part 1

--- src/distress_signal.rs
+++ src/distress_signal.rs
@@ -2,14 +2,13 @@
 
 use std::borrow::Cow;
 use std::cmp::Ordering;
-use std::collections::{HashMap, VecDeque};
 use std::fmt::Debug;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, Write};
 use std::ops::Add;
-
 use itertools::{EitherOrBoth, Itertools};
+
 use ptree::{print_tree, Style, TreeItem};
 use uuid::Uuid;
 
@@ -141,277 +140,57 @@ fn parse_tree(line: &str) -> Node {
     parent
 }
 
-struct Answer {
-    is_in_order: bool,
-    reason: String,
-}
-
-fn double_dfs_recursion(
-    current_l: &Node,
-    current_r: &Node,
-    visited_l: &mut Vec<Uuid>,
-    visited_r: &mut Vec<Uuid>,
-    siblings_counts_l: &HashMap<Uuid, usize>,
-    siblings_counts_r: &HashMap<Uuid, usize>,
-) -> Option<Answer> {
-    if visited_l.contains(&current_l.id) || visited_r.contains(&current_r.id) { // ?
-        return None;
-    }
-    visited_l.push(current_l.id);
-    visited_r.push(current_r.id);
-
-    match (current_l.value, current_r.value) {
+fn compare(curr_l: &Node, curr_r: &Node) -> Ordering {
+    match (curr_l.value, curr_r.value) {
         (Some(val_l), Some(val_r)) => {
-            print!("\t");
-            match val_l.cmp(&val_r) {
-                Ordering::Equal => {
-                    println!("left({:?}) == right({:?}), do nothing...", val_l, val_r);
-                }
-                Ordering::Less => {
-                    let string = format!("CORRECT - because left({}) <= right({})", val_l, val_r);
-                    println!("{}", string);
-                    return Some(Answer { is_in_order: true, reason: string });
-                }
-                Ordering::Greater => {
-                    let string = format!("INCORRECT - because left({}) > right({})", val_l, val_r);
-                    println!("{}", string);
-                    return Some(Answer { is_in_order: false, reason: string });
-                }
-            }
+            // if both have value, then both have no children
+            val_l.cmp(&val_r)
         }
         (None, None) => {
-            println!("left(None) == right(None), do nothing...");
+            for pair in curr_l.children.iter().zip_longest(curr_r.children.iter()) {
+                let result = match pair {
+                    EitherOrBoth::Both(child_l, child_r) => compare(child_l, child_r),
+                    EitherOrBoth::Left(_) => Ordering::Greater, // right run out of items
+                    EitherOrBoth::Right(_) => Ordering::Less, // left run out of items
+                };
+
+                if result == Ordering::Equal {
+                    continue;
+                } else {
+                    return result;
+                }
+            }
+            // empty leaves
+            Ordering::Equal
         }
         (None, Some(val_r)) => {
-            print!("comparing: None with {:?}", current_r);
-            if siblings_counts_r[&current_r.id] == 0 {
-                print!("\t\t{val_r} CAN be converted");
+            let node_copy = Node {
+                id: Default::default(),
+                value: Some(val_r),
+                children: vec![],
+            };
 
-                if current_l.children.iter().filter(|&node| node.value.is_some()).count() == 0 {
-                    let string = "CORRECT - Left side ran out of items, so inputs are in the right order".to_string();
-                    println!("{}", string);
-                    return Some(Answer { is_in_order: true, reason: string });
-                }
+            let extended_child = Node {
+                id: Default::default(),
+                value: None,
+                children: vec![node_copy],
+            };
 
-                for child in current_l.children.iter() {
-                    if let Some(val) = child.value {
-                        match val.cmp(&val_r) {
-                            Ordering::Equal => { break; }
-                            Ordering::Less => {
-                                let string = format!("CORRECT - because left({}) <= right({})", val, val_r);
-                                println!("{}", string);
-                                return Some(Answer { is_in_order: true, reason: string });
-                            }
-                            Ordering::Greater => {
-                                let string = format!("INCORRECT - because left({}) > right({})", val, val_r);
-                                println!("{}", string);
-                                return Some(Answer { is_in_order: false, reason: string });
-                            }
-                        }
-                    } else {
-                        continue;
-                    }
-                }
-            }
-            println!();
+            compare(curr_l, &extended_child)
         }
         (Some(val_l), None) => {
-            print!("comparing: {:?} with None", current_l);
-            if siblings_counts_l[&current_l.id] == 0 {
-                print!("\t\t{val_l} CAN be converted");
-                if current_l.children.iter().filter(|&node| node.value.is_some()).count() == 0 {
-                    let string = "INCORRECT - Right side ran out of items, so inputs are not in the right order".to_string();
-                    println!("{}", string);
-                    return Some(Answer { is_in_order: false, reason: string });
-                }
+            let node_copy = Node {
+                id: Default::default(),
+                value: Some(val_l),
+                children: vec![],
+            };
 
-                for child in current_r.children.iter() {
-                    if let Some(val) = child.value {
-                        match val_l.cmp(&val) {
-                            Ordering::Equal => { break; }
-                            Ordering::Less => {
-                                let string = format!("CORRECT - because left({}) <= right({})", val_l, val);
-                                println!("{}", string);
-                                return Some(Answer { is_in_order: true, reason: string });
-                            }
-                            Ordering::Greater => {
-                                let string = format!("INCORRECT - because left({}) > right({})", val_l, val);
-                                println!("{}", string);
-                                return Some(Answer { is_in_order: false, reason: string });
-                            }
-                        }
-                    } else {
-                        continue;
-                    }
-                }
-            }
-            println!();
-        }
-    }
-
-    for item in current_l.children.iter().zip_longest(current_r.children.iter()) {
-        match item {
-            EitherOrBoth::Both(child_l, child_r) => {
-                let answer = double_dfs_recursion(child_l, child_r, visited_l, visited_r, siblings_counts_l, siblings_counts_r);
-                if answer.is_some() {
-                    return answer;
-                };
-            }
-            EitherOrBoth::Left(_) => {
-                let string = "INCORRECT - Right side ran out of items, so inputs are not in the right order".to_string();
-                println!("{}", string);
-                return Some(Answer { is_in_order: false, reason: string });
-            }
-            EitherOrBoth::Right(_) => {
-                let string = "CORRECT - Left side ran out of items, so inputs are in the right order".to_string();
-                println!("{}", string);
-                return Some(Answer { is_in_order: true, reason: string });
-            }
-        }
-    }
-
-    None
-}
-
-fn dfs_count_siblings(root: &Node) -> HashMap<Uuid, usize> {
-    let mut stack = VecDeque::new();
-    let mut visited = Vec::new();
-
-    let mut siblings_counts = HashMap::new();
-
-    stack.push_front(root);
-
-    while !stack.is_empty() {
-        let current_node = stack.pop_front().unwrap();
-
-        if !visited.contains(&current_node) {
-            visited.push(current_node);
-
-            let siblings = current_node.children.iter().filter(|&node| node.value.is_some()).count();
-
-            for child in current_node.children.iter().rev() {
-                if child.value.is_some() {
-                    siblings_counts.insert(child.id, siblings - 1);
-                }
-                stack.push_front(child);
-            }
-        }
-    }
-
-    siblings_counts
-}
-
-type Depth = usize;
-type SiblingsNum = usize;
-
-fn dfs_leafs_in_order(root: &Node) -> Vec<(i32, Depth, SiblingsNum)> {
-    let mut stack = VecDeque::new();
-    let mut data: Vec<(i32, Depth, SiblingsNum)> = Vec::new();
-
-    stack.push_front((root, 0_usize, root.children.len()));
-
-    while !stack.is_empty() {
-        let (current_node, depth, mut num) = stack.pop_front().unwrap();
-
-        for child in current_node.children.iter().rev() {
-            stack.push_front((child, depth + 1, current_node.children.len()));
-        }
-
-        num = if num > 0 { num - 1 } else { 0 };
-        if let Some(val) = current_node.value {
-            data.push((val as i32, depth, num));
-        } else {
-            data.push((-1, depth, num));
-        }
-    }
-
-    data
-}
-
-fn double_dfs_new(root_l: &Node, root_r: &Node) -> bool {
-    let mut stack_l = VecDeque::new();
-    let mut stack_r = VecDeque::new();
-
-    stack_l.push_front((root_l, 0_usize, root_l.children.len()));
-    stack_r.push_front((root_r, 0_usize, root_l.children.len()));
-
-    let mut must_be_comparable = false;
-    loop {
-        if stack_l.is_empty() {
-            println!("left tree - empty");
-            return true;
-        }
-        if stack_r.is_empty() {
-            println!("right tree - empty");
-            return false;
-        }
-
-        let (curr_l, depth_l, sib_l) = stack_l.pop_front().unwrap();
-        let (curr_r, depth_r, sib_r) = stack_r.pop_front().unwrap();
-
-        for child in curr_l.children.iter().rev() {
-            stack_l.push_front((child, depth_l + 1, curr_l.children.len()));
-        }
-        for child in curr_r.children.iter().rev() {
-            stack_r.push_front((child, depth_r + 1, curr_r.children.len()));
-        }
-
-        println!("current left: {:?} (parent: )\t current right: {:?} (parent: )", curr_l.value, curr_r.value);
-        match (curr_l.value, curr_r.value) {
-            (Some(l), Some(r)) => {
-                must_be_comparable = false;
-
-                if depth_l != depth_r {
-                    println!("WRONG - different depths");
-                    return false;
-                }
-                match l.cmp(&r) {
-                    Ordering::Equal => { continue; }
-                    Ordering::Less => {
-                        println!("CORRECT - because left({}) <= right({})", l, r);
-                        return true;
-                    }
-                    Ordering::Greater => {
-                        println!("WRONG - because left({}) > right({})", l, r);
-                        return false;
-                    }
-                }
-            }
-            (None, None) => {
-                println!("this node_r id: {:?}", curr_r.id);
-                continue;
-            }
-            (Some(_), None) => {
-                if must_be_comparable {
-                    println!("cannot move second time");
-                    return false;
-                }
-
-                if depth_l != depth_r {
-                    println!("WRONG - different depths");
-                    return false;
-                }
-
-                println!("move to next turn");
-                stack_l.push_front((curr_l, depth_l + 1, 0));
-                must_be_comparable = true;
-                continue;
-            }
-            (None, Some(_)) => {
-                if must_be_comparable {
-                    println!("cannot move second time");
-                    return false;
-                }
-                if depth_l != depth_r {
-                    println!("WRONG - different depths");
-                    return false;
-                }
-
-                println!("move to next turn");
-                stack_r.push_front((curr_r, depth_r + 1, 0));
-                must_be_comparable = true;
-                continue;
-            }
+            let extended_child = Node {
+                id: Uuid::new_v4(),
+                value: None,
+                children: vec![node_copy],
+            };
+            compare(&extended_child, curr_r)
         }
     }
 }
@@ -424,70 +203,12 @@ pub fn distress_signal_part_1(file_name: &str) -> usize {
         let _ = print_tree(&node_l);
         let _ = print_tree(&node_r);
 
-        let is_in_order = double_dfs_new(&node_l, &node_r);
-        println!("pair nr {index}");
-        if is_in_order {
+        let is_in_order = compare(&node_l, &node_r);
+
+        if is_in_order == Ordering::Equal || is_in_order == Ordering::Less {
             indices_sum += index + 1
         }
-        // let leafs_in_order_l = dfs_leafs_in_order(&node_l);
-        // let leafs_in_order_r = dfs_leafs_in_order(&node_r);
-        // println!("{:?}\n", leafs_in_order_l);
-        // println!("{:?}\n", leafs_in_order_r);
-
-
-        // let is_in_oreder: bool = compare(leafs_in_order_l, leafs_in_order_r);
-
-        // let siblings_count_l = dfs_count_siblings(&node_l);
-        // let siblings_count_r = dfs_count_siblings(&node_r);
-        //
-        // for (id, num) in siblings_count_l.iter() {
-        //     print!("({}: {}) ", &id.to_string()[..3], num);
-        // }
-        // println!();
-        // for (id, num) in siblings_count_r.iter() {
-        //     print!("({}: {}) ", &id.to_string()[..3], num);
-        // }
-        // println!();
-        //
-        // let mut visited_l = Vec::new();
-        // let mut visited_r = Vec::new();
-        //
-        // let answer = double_dfs_recursion(&node_l, &node_r, &mut visited_l, &mut visited_r, &siblings_count_l, &siblings_count_r);
-        // if answer.unwrap().is_in_order {
-        //     indices_sum += index + 1
-        // }
     }
 
     indices_sum
-}
-
-fn compare(leafs_l: &[(i32, Depth, SiblingsNum)], leafs_r: &[(i32, Depth, SiblingsNum)]) -> bool {
-    for item in leafs_l.iter().zip_longest(leafs_r.iter()) {
-        match item {
-            EitherOrBoth::Both((val_l, depth_l, sib_l), (val_r, depth_r, sib_r)) => {
-                // if *val_l < 0 && *val_r > 0 {
-                //     if val_
-                // }
-
-                if *depth_l == *depth_r {
-                    // compare val_l with val_r
-                    continue;
-                }
-
-                if depth_l.abs_diff(*depth_r) == 1 {}
-            }
-            EitherOrBoth::Left(_) => {
-                let string = "INCORRECT - Right side ran out of items, so inputs are not in the right order".to_string();
-                println!("{}", string);
-                // return Some(Answer { is_in_order: false, reason: string });
-            }
-            EitherOrBoth::Right(_) => {
-                let string = "CORRECT - Left side ran out of items, so inputs are in the right order".to_string();
-                println!("{}", string);
-                // return Some(Answer { is_in_order: true, reason: string });
-            }
-        }
-    }
-
-    todo!()
 }
