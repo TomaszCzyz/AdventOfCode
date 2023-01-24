9a5a41752937b2a0baf804ee1a33005bd40695bd
tczyz <tomczyz97@gmail.com>
2023-01-24 03:07:24 +0100

    add partial solution for task 18 part 2

--- src/boiling_boulders.rs
+++ src/boiling_boulders.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)]
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Debug)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct Point {
     x: u32,
     y: u32,
@@ -40,9 +40,7 @@ fn read_input(file_name: &str) -> Vec<Point> {
     points
 }
 
-pub fn boiling_boulders_part_1(file_name: &str) -> usize {
-    let points = read_input(file_name);
-
+fn initialize_mappings(points: Vec<Point>) -> [HashMap<(u32, u32), Vec<u32>>; 3] {
     let mut xy_to_z_map: HashMap<(u32, u32), Vec<u32>> = HashMap::new();
     let mut xz_to_y_map: HashMap<(u32, u32), Vec<u32>> = HashMap::new();
     let mut yz_to_x_map: HashMap<(u32, u32), Vec<u32>> = HashMap::new();
@@ -52,17 +50,26 @@ pub fn boiling_boulders_part_1(file_name: &str) -> usize {
         xz_to_y_map.entry((p.x, p.z)).or_insert_with(Vec::new).push(p.y);
         yz_to_x_map.entry((p.y, p.z)).or_insert_with(Vec::new).push(p.x);
     }
+    [xy_to_z_map, xz_to_y_map, yz_to_x_map]
+}
+
+pub fn boiling_boulders_part_1(file_name: &str) -> usize {
+    let points = read_input(file_name);
+    let mappings = initialize_mappings(points);
 
     let mut visible_sides = 0_usize;
-    for map in [xy_to_z_map, xz_to_y_map, yz_to_x_map].iter() {
+    for map in mappings.iter() {
         let mut visible_sides_per_plane = 0_usize;
-        for mut cubes_indexes in map.values().cloned() {
-            cubes_indexes.sort();
+        for (coords, cubes_indexes) in map.iter() {
+            let mut indexes = cubes_indexes.clone();
+            indexes.sort();
+
+            println!("{:?}: {:?}", coords, indexes);
 
             let mut visible_counter = 2_usize;
 
-            if cubes_indexes.len() != 1 {
-                for consecutive_indexes in cubes_indexes.windows(2) {
+            if indexes.len() != 1 {
+                for consecutive_indexes in indexes.windows(2) {
                     if consecutive_indexes[1] != consecutive_indexes[0] + 1 {
                         visible_counter += 2;
                     }
@@ -72,9 +79,7 @@ pub fn boiling_boulders_part_1(file_name: &str) -> usize {
             visible_sides_per_plane += visible_counter;
         }
 
-        // println!("{map:?}");
-        // println!("visible sides {:?}", visible_sides_per_plane);
-
+        println!("visible sides {:?}", visible_sides_per_plane);
         visible_sides += visible_sides_per_plane;
     }
 
@@ -82,5 +87,61 @@ pub fn boiling_boulders_part_1(file_name: &str) -> usize {
 }
 
 pub fn boiling_boulders_part_2(file_name: &str) -> usize {
-    0
+    let points = read_input(file_name);
+    let mappings = initialize_mappings(points);
+
+    let mut visible_sides = 0_usize;
+    let mut gaps = Vec::new();
+    for (plane_number, map) in mappings.iter().enumerate() {
+        let mut visible_sides_per_plane = 0_usize;
+        let mut gaps_per_plane = HashSet::new();
+
+        for (coords, cubes_indexes) in map.iter() {
+            let mut indexes = cubes_indexes.clone();
+            indexes.sort();
+
+            println!("{:?}: {:?}", coords, indexes);
+
+            let mut visible_counter = 2_usize;
+            for consecutive_indexes in indexes.windows(2) {
+                let first = consecutive_indexes[0];
+                let second = consecutive_indexes[1];
+
+                if second == first + 1 {
+                    continue;
+                }
+
+                visible_counter += 2;
+
+                for i in first + 1..second {
+                    gaps_per_plane.insert(match plane_number {
+                        0 => Point { x: coords.0, y: coords.1, z: i },
+                        1 => Point { x: coords.0, y: i, z: coords.1 },
+                        2 => Point { x: i, y: coords.0, z: coords.1 },
+                        _ => panic!()
+                    });
+                }
+            }
+
+            visible_sides_per_plane += visible_counter;
+        }
+
+        println!("gaps {:?}", gaps_per_plane);
+        println!("visible sides {:?}", visible_sides_per_plane);
+
+        visible_sides += visible_sides_per_plane;
+        gaps.push(gaps_per_plane);
+    }
+
+    let gaps_intersection = gaps.iter()
+        .skip(1)
+        .fold(HashSet::from_iter(gaps[0].clone()), |acc, set| &acc & set);
+
+    println!("gaps_intersection {gaps_intersection:?}");
+
+    // find all distinct droplets
+    // do part_1 of the solution for each
+    // subtract results from initial sum
+
+    visible_sides
 }
