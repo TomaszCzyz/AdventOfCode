ced6488d493b99cfcf116d308303f270a43e0909
tczyz <tomczyz97@gmail.com>
2023-02-15 18:52:49 +0100

    add third unfinished solution for task 19 part 1

--- src/not_enough_minerals.rs
+++ src/not_enough_minerals.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+use itertools::Itertools;
 use strum::IntoEnumIterator;
 use strum_macros::{Display, EnumIter};
 
@@ -17,13 +18,12 @@ enum Mineral {
     Geode,
 }
 
-type CostInfo = (Mineral, u32);
-
+type CostInfo = HashMap<Mineral, u32>;
 
 #[derive(Debug)]
 struct Blueprint {
     id: usize,
-    costs: HashMap<Mineral, Vec<CostInfo>>,
+    costs: HashMap<Mineral, CostInfo>,
 }
 
 fn read_input(file_name: &str) -> Vec<Blueprint> {
@@ -58,10 +58,10 @@ fn read_input(file_name: &str) -> Vec<Blueprint> {
             .collect::<Vec<_>>();
 
         let id = data[0] as usize;
-        let ore_robot_costs = vec![(Mineral::Ore, data[1])];
-        let clay_robot_costs = vec![(Mineral::Ore, data[2])];
-        let obsidian_robot_costs = vec![(Mineral::Ore, data[3]), (Mineral::Clay, data[4])];
-        let geode_robot_costs = vec![(Mineral::Ore, data[5]), (Mineral::Obsidian, data[6])];
+        let ore_robot_costs = HashMap::from([(Mineral::Ore, data[1])]);
+        let clay_robot_costs = HashMap::from([(Mineral::Ore, data[2])]);
+        let obsidian_robot_costs = HashMap::from([(Mineral::Ore, data[3]), (Mineral::Clay, data[4])]);
+        let geode_robot_costs = HashMap::from([(Mineral::Ore, data[5]), (Mineral::Obsidian, data[6])]);
 
         costs.insert(Mineral::Ore, ore_robot_costs);
         costs.insert(Mineral::Clay, clay_robot_costs);
@@ -125,6 +125,142 @@ fn mine_minerals(robots: &HashMap<Mineral, u32>, minerals: &mut HashMap<Mineral,
     }
 }
 
+fn try_build_next_robots(
+    minute: u32,
+    costs: &HashMap<Mineral, CostInfo>,
+    robots: HashMap<Mineral, u32>,
+    minerals: HashMap<Mineral, u32>,
+    history: HashMap<Mineral, Vec<u32>>,
+    robots_to_build: Vec<Mineral>,
+) -> u32 {
+    if minute == TOTAL_TIME {
+        print_status(&robots, &minerals, &history);
+
+        return minerals[&Mineral::Geode];
+    }
+
+    let geode_obsidian_cost = &costs[&Mineral::Geode][&Mineral::Obsidian];
+    if minute >= TOTAL_TIME - geode_obsidian_cost && !history.contains_key(&Mineral::Obsidian) {
+        // we wont be able to build any geode robots till 24 minute
+        return 0;
+    }
+
+    let mut answers = Vec::new();
+
+    // try to build requested robots
+    for robot_type in robots_to_build.iter() {
+        let robot_costs = &costs[robot_type];
+        let mut is_affordable = true;
+
+        // check if there is enough minerals
+        for (mineral, cost) in robot_costs.iter() {
+            if minerals[mineral] < *cost {
+                is_affordable = false;
+                break;
+            }
+        }
+
+        if !is_affordable {
+            // postpone the same build list to next minute
+            let mut updated_minerals = minerals.clone();
+            let updated_robots = robots.clone();
+
+            mine_minerals(&updated_robots, &mut updated_minerals);
+
+            let answer = try_build_next_robots(
+                minute + 1,
+                costs,
+                updated_robots,
+                updated_minerals,
+                history.clone(),
+                robots_to_build.clone());
+
+            answers.push(answer);
+        } else { // is affordable
+            let mut updated_history = history.clone();
+            let mut updated_minerals = minerals.clone();
+            let mut updated_robots = robots.clone();
+
+            updated_history.entry(*robot_type)
+                .and_modify(|minutes| minutes.push(minute))
+                .or_insert_with(|| vec![minute]);
+
+            // spend minerals to build a robot
+            for (mineral, cost) in robot_costs.iter() {
+                updated_minerals.entry(*mineral).and_modify(|qty| *qty -= cost);
+            }
+
+            // mine new minerals
+            mine_minerals(&updated_robots, &mut updated_minerals);
+
+            // finish construction of a new robot
+            updated_robots.entry(*robot_type).and_modify(|robots_number| *robots_number += 1);
+
+            // make new build request
+            let new_robots_to_build: Vec<Mineral> = create_build_request(*robot_type, minute, &updated_history);
+
+            let answer = try_build_next_robots(
+                minute + 1,
+                costs,
+                updated_robots,
+                updated_minerals,
+                updated_history,
+                new_robots_to_build);
+
+            answers.push(answer);
+        }
+    }
+
+    *answers.iter().max().unwrap()
+}
+
+fn create_build_request(robot_type: Mineral, current_minute: u32, history: &HashMap<Mineral, Vec<u32>>) -> Vec<Mineral> {
+    match robot_type {
+        Mineral::Ore => vec![Mineral::Ore, Mineral::Clay],
+        Mineral::Clay => vec![Mineral::Clay, Mineral::Obsidian],
+        Mineral::Obsidian => vec![Mineral::Obsidian, Mineral::Geode],
+        Mineral::Geode => vec![Mineral::Geode],
+    }
+    // match (history.contains_key(&Mineral::Ore), history.contains_key(&Mineral::Clay), history.contains_key(&Mineral::Obsidian), history.contains_key(&Mineral::Geode)) {
+    //     (true, true, true, true) => vec![Mineral::Geode],
+    //     (true, true, true, false) => {
+    //         let most_recent_clay = history[&Mineral::Clay].iter().last().unwrap();
+    //         if current_minute < most_recent_clay + 3 {
+    //             vec![Mineral::Clay, Mineral::Obsidian, Mineral::Geode]
+    //         } else {
+    //             vec![Mineral::Obsidian, Mineral::Geode]
+    //         }
+    //     }
+    //     (true, true, false, false) => {
+    //         let most_recent_ore = history[&Mineral::Ore].iter().last().unwrap();
+    //         if current_minute < most_recent_ore + 3 {
+    //             vec![Mineral::Ore, Mineral::Clay, Mineral::Obsidian]
+    //         } else {
+    //             vec![Mineral::Obsidian, Mineral::Geode]
+    //         }
+    //     }
+    //     (true, false, false, false) => {
+    //         vec![Mineral::Ore, Mineral::Clay]
+    //     }
+    //     _ => {
+    //         vec![Mineral::Ore, Mineral::Clay]
+    //     }
+    // }
+}
+
+fn print_status(robots: &HashMap<Mineral, u32>, minerals: &HashMap<Mineral, u32>, history: &HashMap<Mineral, Vec<u32>>) {
+    println!("FINISHED");
+    for (val, min) in history.iter()
+        .flat_map(|(key, values)| {
+            values.iter().map(move |value| (key, *value))
+        })
+        .sorted_by(|(_, min1), (_, min2)| min1.cmp(min2))
+        .collect::<Vec<_>>() {
+        println!("minute {min} - build {val} robot");
+    }
+    print_equipment(robots, minerals);
+}
+
 /// round phases:
 /// 1. spending minerals for robots construction
 /// 1. mining minerals
@@ -140,8 +276,9 @@ fn analyze_next_minute(
         println!("FINISHED");
         for (mineral, min) in history {
             match mineral {
-                // None => println!("minute {min} - do nothing"),
-                None => {}
+                None => {
+                    // println!("minute {min} - do nothing"),
+                }
                 Some(val) => println!("minute {min} - build {val} robot"),
             }
         }
@@ -154,24 +291,32 @@ fn analyze_next_minute(
         return 0;
     }
 
-    let mut allowed_minerals = vec![Mineral::Geode, Mineral::Obsidian, Mineral::Clay, Mineral::Ore];
-    for (mineral, min) in history.iter() {
-        if let Some(val) = mineral {
-            match val {
-                Mineral::Geode => {
-                    allowed_minerals = vec![Mineral::Obsidian];
-                }
-                Mineral::Obsidian => {
-                    allowed_minerals = vec![Mineral::Geode, Mineral::Obsidian, Mineral::Clay];
-                }
-                Mineral::Clay => {}
-                Mineral::Ore => {}
+    let mut allowed_minerals: Vec<Mineral> = vec![Mineral::Geode, Mineral::Obsidian, Mineral::Clay, Mineral::Ore];
+
+    let filtered_history = history.iter()
+        .filter(|(miner, min)| miner.is_some())
+        .map(|(miner, min)| (miner.unwrap(), min));
+
+    match filtered_history.clone().last() {
+        None => { // no robot was build yet
+            allowed_minerals = vec![Mineral::Clay, Mineral::Ore]
+        }
+        Some((miner, _min)) => {
+            allowed_minerals = match miner {
+                Mineral::Ore => vec![Mineral::Clay, Mineral::Ore],
+                Mineral::Clay => vec![Mineral::Obsidian, Mineral::Clay],
+                Mineral::Obsidian => vec![Mineral::Geode, Mineral::Obsidian],
+                Mineral::Geode => vec![Mineral::Geode],
             }
+
+            // append "lower" mineral if...
         }
     }
 
-    let mut answers = Vec::new();
+    println!("\tallowed minerals in minute {minute} are: {:?}", allowed_minerals);
 
+    let mut answers = Vec::new();
+    let mut was_anything_build = false;
 
     // choice: try buy a robot
     for mineral in allowed_minerals {
@@ -187,6 +332,7 @@ fn analyze_next_minute(
         }
 
         if is_affordable {
+            was_anything_build = true;
             let mut updated_history = history.clone();
             let mut updated_minerals = minerals.clone();
             let mut updated_robots = robots.clone();
@@ -209,17 +355,30 @@ fn analyze_next_minute(
         }
     }
 
-    // choice: do nothing
-    let mut updated_minerals = minerals;
-    let mut updated_history = history.clone();
+    if !was_anything_build {
+        // choice: do nothing
+        let mut updated_minerals = minerals;
+        let mut updated_history = history.clone();
 
-    updated_history.push((None, minute));
+        updated_history.push((None, minute));
 
+        mine_minerals(&robots, &mut updated_minerals);
 
-    mine_minerals(&robots, &mut updated_minerals);
+        let answer = analyze_next_minute(minute + 1, robots.clone(), updated_minerals, costs, updated_history);
+        answers.push(answer);
+    }
 
-    let answer = analyze_next_minute(minute + 1, robots.clone(), updated_minerals, costs, updated_history);
-    answers.push(answer);
+    // // choice: do nothing
+    // let mut updated_minerals = minerals;
+    // let mut updated_history = history.clone();
+    //
+    // updated_history.push((None, minute));
+    //
+    //
+    // mine_minerals(&robots, &mut updated_minerals);
+    //
+    // let answer = analyze_next_minute(minute + 1, robots.clone(), updated_minerals, costs, updated_history);
+    // answers.push(answer);
 
     *answers.iter().max().unwrap()
 }
@@ -230,7 +389,15 @@ pub fn not_enough_minerals_part_1(file_name: &str) -> u32 {
 
     println!("{:#?}", costs);
 
-    let max_geode_qty = analyze_next_minute(1, robots, minerals, &costs[0], Vec::new());
+    // let max_geode_qty = analyze_next_minute(1, robots, minerals, &costs[0], Vec::new());
+    let max_geode_qty = try_build_next_robots(
+        1,
+        &costs[0].costs,
+        robots,
+        minerals,
+        HashMap::from([(Mineral::Ore, vec![0])]),
+        Vec::from([Mineral::Clay, Mineral::Ore]));
+
     println!("max geode qty for blueprint {} is {}", costs[0].id, max_geode_qty);
     max_geode_qty
 }
