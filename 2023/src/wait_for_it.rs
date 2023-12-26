cb3816951134dc16b7cffee8c64bd8775c2a35bf
tczyz <tomczyz97@gmail.com>
2023-12-26 02:14:41 +0100

    Add solution for task 6 part 2

--- 2023/src/wait_for_it.rs
+++ 2023/src/wait_for_it.rs
@@ -1,4 +1,3 @@
-use std::fs::read;
 use std::io::BufRead;
 
 pub fn read_input(file_name: &str) -> Vec<(usize, usize)> {
@@ -28,15 +27,15 @@ pub fn read_input(file_name: &str) -> Vec<(usize, usize)> {
     times.into_iter().zip(distances).collect::<Vec<(usize, usize)>>()
 }
 
-fn calc_quadratic_solutions(t: f32, s: f32) -> (f32, f32) {
-    let square_delta = f32::sqrt(f32::powi(t, 2) - 4. * s);
+fn calc_quadratic_solutions(t: f64, s: f64) -> (f64, f64) {
+    let square_delta = f64::sqrt(f64::powf(t, 2.) - 4. * s);
     let t1 = (t - square_delta) / 2.;
     let t2 = (t + square_delta) / 2.;
 
     (t1, t2)
 }
 
-fn count_integers_in_interval(start: f32, end: f32) -> usize {
+fn count_integers_in_interval(start: f64, end: f64) -> usize {
     let start_int = start.ceil() as usize;
     let end_int = end.floor() as usize;
 
@@ -61,26 +60,35 @@ fn wait_for_it_part_1(filename: &str) -> usize {
     let input = read_input(filename);
 
     input.iter()
-        .map(|(t, s)| calc_quadratic_solutions(*t as f32, *s as f32))
+        .map(|(t, s)| calc_quadratic_solutions(*t as f64, *s as f64))
         .map(|(start, end)| count_integers_in_interval(start, end))
         .fold(1, |acc, x| acc * x)
 }
 
 
 fn wait_for_it_part_2(filename: &str) -> usize {
-    todo!()
+    let input = read_input(filename);
+
+    let time = input.iter()
+        .map(|(t, _)| t.to_string())
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+
+    let distance = input.iter()
+        .map(|(_, s)| s.to_string())
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+
+    let (start, end) = calc_quadratic_solutions(time as f64, distance as f64);
+
+    count_integers_in_interval(start, end)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn read_input_test() {
-        let input = read_input("inputs/6_input_example.txt");
-
-        println!("seeds: {input:?}");
-    }
 
     #[test]
     fn part_1_input_example() {
@@ -103,7 +111,7 @@ mod tests {
         let answer = wait_for_it_part_2("inputs/6_input_example.txt");
 
         println!("part 2 - example - answer: {:?}", answer);
-        assert_eq!(answer, 46);
+        assert_eq!(answer, 71503);
     }
 
     #[test]
@@ -111,6 +119,6 @@ mod tests {
         let answer = wait_for_it_part_2("inputs/6_input.txt");
 
         println!("part 2 - original - answer: {:?}", answer);
-        assert_eq!(answer, 0);
+        assert_eq!(answer, 28360140);
     }
 }
