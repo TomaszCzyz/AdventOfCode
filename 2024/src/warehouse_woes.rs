c96189a752beed5faac658ea98d4c28bcae70f70
tczyz <tomczyz97@gmail.com>
2025-03-29 13:37:22 +0100

    Add solution for day 15 part 2

--- 2024/src/warehouse_woes.rs
+++ 2024/src/warehouse_woes.rs
@@ -132,7 +132,6 @@ fn read_input_2(file_name: &str) -> (HashMap<Position, usize>, Position, Vec<Dir
         .enumerate()
         .take_while(|(_, line)| !line.is_empty())
         .for_each(|(row_i, line)| {
-            let mut row_obstacles_num = 0;
             line.chars().enumerate().for_each(|(col_i, c)| match c {
                 'O' => {
                     obstacles.insert(
@@ -150,7 +149,6 @@ fn read_input_2(file_name: &str) -> (HashMap<Position, usize>, Position, Vec<Dir
                         obstacle_num + 1,
                     );
                     obstacle_num += 2;
-                    row_obstacles_num += 1;
                 }
                 '#' => {
                     obstacles.insert(
@@ -171,7 +169,7 @@ fn read_input_2(file_name: &str) -> (HashMap<Position, usize>, Position, Vec<Dir
                 '@' => {
                     robot_pos = Position {
                         row: row_i,
-                        col: 2 * row_obstacles_num + 1 + col_i,
+                        col: 2 * col_i,
                     };
                 }
                 '.' => {}
@@ -188,7 +186,6 @@ fn warehouse_woes_part_1(filename: &str) -> usize {
     let (mut map, mut robot_pos, directions) = read_input(filename);
 
     for direction in directions {
-        // println!("Move {:}:", direction);
         try_move(&mut map, &mut robot_pos, &direction, true);
     }
     print_map(&map, &robot_pos);
@@ -199,14 +196,9 @@ fn warehouse_woes_part_1(filename: &str) -> usize {
         .sum()
 }
 
-fn get_map_boundaries(map: &HashMap<Position, ObstacleType>) -> (usize, usize) {
+fn print_map(map: &HashMap<Position, ObstacleType>, robot_pos: &Position) {
     let max_row = map.keys().map(|p| p.row).max().unwrap_or(0);
     let max_col = map.keys().map(|p| p.col).max().unwrap_or(0);
-    (max_row, max_col)
-}
-
-fn print_map(map: &HashMap<Position, ObstacleType>, robot_pos: &Position) {
-    let (max_row, max_col) = get_map_boundaries(map);
 
     for row in 0..=max_row {
         for col in 0..=max_col {
@@ -240,6 +232,7 @@ fn print_map_2(map: &HashMap<Position, usize>, robot_pos: &Position) {
                 match obstacle {
                     0 => print!("#"),
                     x => {
+                        // print!("{}", x);
                         if x % 2 == 0 {
                             print!("]");
                         } else {
@@ -296,103 +289,116 @@ fn try_move(
     }
 }
 
-fn warehouse_woes_part_2(filename: &str) -> u64 {
-    let (map, robot_pos, directions) = read_input_2(filename);
+fn warehouse_woes_part_2(filename: &str) -> usize {
+    let (mut map, mut robot_pos, directions) = read_input_2(filename);
+    print_map_2(&map, &robot_pos);
 
     for direction in directions {
-        // try_move(&mut map, &mut robot_pos, &direction, true);
-        // println!("Move {:}:", direction);
-        print_map_2(&map, &robot_pos);
+        let mut boxes_to_move = Vec::new();
+        if can_move_robot(&map, &robot_pos, &direction, &mut boxes_to_move) {
+            let boxes_to_update = boxes_to_move
+                .iter()
+                .map(|box_num| {
+                    map.iter()
+                        .find(|&(_, num)| *num == *box_num)
+                        .map(|(key, value)| ((*key).clone(), *value))
+                        .unwrap()
+                })
+                .collect::<Vec<_>>();
+
+            for (box_pos, _) in &boxes_to_update {
+                map.remove_entry(&box_pos);
+            }
+
+            for (box_pos, box_num) in &boxes_to_update {
+                map.insert(box_pos.move_to(&direction), *box_num);
+            }
+
+            robot_pos = robot_pos.move_to(&direction);
+        }
     }
 
     print_map_2(&map, &robot_pos);
-    todo!()
+
+    map.iter()
+        .filter(|&(_, num)| *num != 0 && *num % 2 == 1)
+        .map(|(pos, _)| pos.row * 100 + pos.col)
+        .sum()
 }
 
 fn can_move_robot(
-    map: &mut HashMap<Position, usize>,
+    map: &HashMap<Position, usize>,
     current_pos: &Position,
     direction: &Direction,
+    boxes_to_move: &mut Vec<usize>,
 ) -> bool {
     let next_position = current_pos.move_to(direction);
 
     match map.get(&next_position) {
         None => true,
         Some(obstacle_num) => {
-            let mut obstacle_pos = next_position;
+            let obstacle_pos = next_position;
             if *obstacle_num == 0 {
                 false
             } else {
-                can_move_box(map, &mut obstacle_pos, direction)
+                can_move_box(map, &obstacle_pos, direction, boxes_to_move)
             }
         }
     }
 }
 
 fn can_move_box(
-    map: &mut HashMap<Position, usize>,
+    map: &HashMap<Position, usize>,
     current_pos: &Position,
     direction: &Direction,
+    boxes_to_move: &mut Vec<usize>,
 ) -> bool {
     let box_positions = get_box_positions(map, &current_pos);
-    
-    match direction {
-        Direction::Up => {
-            
-        }
-        Direction::Down => {}
-        Direction::Left => {}
-        Direction::Right => {}
-    }
-    
-    let next_position = current_pos.move_to(direction);
+    boxes_to_move.push(*map.get(&box_positions[0]).unwrap());
+    boxes_to_move.push(*map.get(&box_positions[1]).unwrap());
 
-    match map.get(&next_position) {
-        None => true,
-        Some(obstacle_num) => {
-            let mut obstacle_pos = next_position;
-            if *obstacle_num == 0 {
-                false
-            } else {
-                let can_move = try_move(map, &mut obstacle_pos, direction, false);
-                if can_move {
-                    map.remove_entry(&obstacle_pos);
-                    map.insert(obstacle_pos.move_to(direction), ObstacleType::Box);
+    let positions_to_check = match direction {
+        Direction::Left => vec![box_positions[0].move_to(direction)],
+        Direction::Right => vec![box_positions[1].move_to(direction)],
+        Direction::Up | Direction::Down => vec![
+            box_positions[0].move_to(direction),
+            box_positions[1].move_to(direction),
+        ],
+    };
 
-                    if is_robot {
-                        *current_pos = obstacle_pos;
-                    }
-                    true
-                } else {
+    positions_to_check
+        .iter()
+        .map(|pos| {
+            return if let Some(obstacle_num) = map.get(&pos) {
+                if *obstacle_num == 0 {
                     false
+                } else {
+                    can_move_box(map, &pos, direction, boxes_to_move)
                 }
-            }
-        }
-    }
+            } else {
+                true
+            };
+        })
+        .all(|x| x)
 }
 
-fn get_box_positions(map: &mut HashMap<Position, usize>, current_pos: &Position) -> [Position; 2] {
-    let pos_to_left = Position {
-        row: current_pos.row,
-        col: current_pos.col - 1,
-    };
+/// Gets Positions of box fields, i.e. '[' and ']'
+///
+/// returns: [Position; 2]
+fn get_box_positions(map: &HashMap<Position, usize>, current_pos: &Position) -> [Position; 2] {
+    if map.get(&current_pos).unwrap() % 2 == 1 {
+        let pos_to_right = Position {
+            row: current_pos.row,
+            col: current_pos.col + 1,
+        };
 
-    let pos_to_right = Position {
-        row: current_pos.row,
-        col: current_pos.col + 1,
-    };
-
-    let box_num = map.get(&current_pos).unwrap();
-    let box_num_to_left = map.get(&pos_to_left);
-
-    if let Some(box_num_to_left) = box_num_to_left {
-        if box_num_to_left == box_num {
-            [pos_to_left, *current_pos]
-        } else {
-            [*current_pos, pos_to_right]
-        }
+        [*current_pos, pos_to_right]
     } else {
-        unreachable!()
+        let pos_to_left = Position {
+            row: current_pos.row,
+            col: current_pos.col - 1,
+        };
+        [pos_to_left, *current_pos]
     }
 }
 
@@ -427,7 +433,15 @@ mod tests {
         let answer = warehouse_woes_part_2("inputs/15_input_example_3.txt");
 
         println!("part 2 - example - answer: {:?}", answer);
-        assert_eq!(answer, 875318608908);
+        assert_eq!(answer, 618);
+    }
+
+    #[test]
+    fn part_2_input_example_2() {
+        let answer = warehouse_woes_part_2("inputs/15_input_example_2.txt");
+
+        println!("part 2 - example - answer: {:?}", answer);
+        assert_eq!(answer, 9021);
     }
 
     #[test]
@@ -435,6 +449,6 @@ mod tests {
         let answer = warehouse_woes_part_2("inputs/15_input.txt");
 
         println!("part 2 - original - answer: {:?}", answer);
-        assert_eq!(answer, 99423413811305);
+        assert_eq!(answer, 1489116);
     }
 }
