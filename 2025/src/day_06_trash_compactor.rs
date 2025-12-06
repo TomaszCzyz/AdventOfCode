a3d28a578c5e74d3fd8da5327d52c58c2e969fa6
tczyz <tomczyz97@gmail.com>
2025-12-06 20:53:33 +0100

    Add a solution for day 6 part 2

--- 2025/src/day_06_trash_compactor.rs
+++ 2025/src/day_06_trash_compactor.rs
@@ -20,15 +20,7 @@ fn read_input(file_name: &str) -> (Vec<Vec<i64>>, Vec<Operation>) {
 
             lines.push(numbers);
         } else {
-            let operations = line
-                .split_whitespace()
-                .map(|s| match s {
-                    "+" => Operation::Add,
-                    "*" => Operation::Multiply,
-                    _ => panic!("unknown operation: {}", s),
-                })
-                .collect::<Vec<_>>();
-
+            let operations = parse_operations(line);
             return (lines, operations);
         }
     }
@@ -36,11 +28,38 @@ fn read_input(file_name: &str) -> (Vec<Vec<i64>>, Vec<Operation>) {
     unreachable!();
 }
 
+fn read_input_2(file_name: &str) -> (Vec<Vec<u8>>, Vec<Operation>) {
+    let file_content = fs::read_to_string(file_name).unwrap();
+
+    let mut lines = Vec::new();
+    for line in file_content.lines() {
+        let first_char = line.chars().next().unwrap();
+        if first_char.is_digit(10) || first_char.is_whitespace() {
+            lines.push(line.as_bytes().to_vec());
+        } else {
+            let operations = parse_operations(line);
+            return (lines, operations);
+        }
+    }
+
+    unreachable!();
+}
+
+fn parse_operations(line: &str) -> Vec<Operation> {
+    line.split_whitespace()
+        .map(|s| match s {
+            "+" => Operation::Add,
+            "*" => Operation::Multiply,
+            _ => panic!("unknown operation: {}", s),
+        })
+        .collect::<Vec<_>>()
+}
+
 fn part_1(filename: &str) -> i64 {
     let (numbers_lines, operations) = read_input(filename);
     let numbers_lines = transpose(numbers_lines);
 
-    let mut total = 0 as i64;
+    let mut total = 0i64;
     for (i, op) in operations.iter().enumerate() {
         total += match op {
             Operation::Add => numbers_lines[i].iter().sum::<i64>(),
@@ -51,10 +70,60 @@ fn part_1(filename: &str) -> i64 {
     total
 }
 
-fn part_2(filename: &str) -> usize {
-    let _ = read_input(filename);
+fn part_2(filename: &str) -> i64 {
+    let (mut rows, operations) = read_input_2(filename);
 
-    todo!()
+    let max_width = rows.iter().map(|a| a.len()).max().unwrap();
+    for rows in rows.iter_mut() {
+        rows.resize(max_width + 1, b'0');
+    }
+
+    let mut is_digit_in_column = true;
+    let mut index = 0;
+    let mut column_index = 0;
+    let mut total = 0;
+
+    while index < max_width {
+        // do calculation for all numbers in the column (set of number separated by whitespace column)
+        let mut column_numbers = Vec::new();
+
+        while is_digit_in_column {
+            is_digit_in_column = false;
+            if index == max_width {
+                break;
+            }
+
+            // create a number from the current column index
+            let mut digits = Vec::new();
+            for line in rows.iter() {
+                let char = line[index];
+                if char.is_ascii_digit() {
+                    is_digit_in_column = true;
+                    digits.push(char - b'0');
+                }
+            }
+
+            if is_digit_in_column {
+                let number = digits
+                    .iter()
+                    .fold(0i64, |acc, &digit| acc * 10 + digit as i64);
+
+                column_numbers.push(number);
+            }
+
+            index += 1;
+        }
+
+        is_digit_in_column = true;
+        let column_total = match operations[column_index] {
+            Operation::Add => column_numbers.iter().sum::<i64>(),
+            Operation::Multiply => column_numbers.iter().product(),
+        };
+        total += column_total;
+        column_index += 1;
+    }
+
+    total
 }
 
 fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
@@ -101,10 +170,10 @@ mod tests {
 
     #[test]
     fn part_2_input_example_1() {
-        let answer = part_2("inputs/06_input_example.txt");
+        let answer = part_2("inputs/06_input_example_1.txt");
 
         println!("part 2 - example - answer: {:?}", answer);
-        assert_eq!(answer, 14);
+        assert_eq!(answer, 3263827);
     }
 
     #[test]
@@ -112,6 +181,6 @@ mod tests {
         let answer = part_2("inputs/06_input.txt");
 
         println!("part 2 - example - answer: {:?}", answer);
-        assert_eq!(answer, 353507173555373);
+        assert_eq!(answer, 9170286552289);
     }
 }
