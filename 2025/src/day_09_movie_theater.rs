91b37bdfb2d2ff42c7c1b76082930a73ffc048c6
Tomasz Czyz <tomasz.czyz@allegro.com>
2025-12-09 18:38:06 +0100

    Add solution for day 9 part 1

--- 2025/src/day_09_movie_theater.rs
+++ 2025/src/day_09_movie_theater.rs
@@ -1,6 +1,8 @@
 use std::fs;
 
-fn read_input(file_name: &str) -> Vec<usize> {
+type Coord = (i64, i64);
+
+fn read_input(file_name: &str) -> Vec<Coord> {
     fs::read_to_string(file_name)
         .unwrap()
         .lines()
@@ -9,17 +11,36 @@ fn read_input(file_name: &str) -> Vec<usize> {
                 .split(',')
                 .map(|s| s.parse::<i64>().unwrap())
                 .collect::<Vec<_>>();
-            (parts[0], parts[1], parts[2])
+            (parts[0], parts[1])
         })
-        .collect::<Vec<_>>();
-
-    todo!()
+        .collect::<Vec<_>>()
 }
 
-fn part_1(filename: &str, laps: usize) -> usize {
-    let _ = read_input(filename);
+fn part_1(filename: &str) -> i64 {
+    let coords = read_input(filename);
 
-    todo!()
+    let mut current_best = 0;
+
+    for i in 0..coords.len() {
+        for j in 0..coords.len() {
+            if i == j {
+                continue;
+            }
+
+            let a = area(coords[i], coords[j]);
+            if a > current_best {
+                current_best = a;
+            }
+        }
+    }
+
+    current_best
+}
+
+fn area(c1: Coord, c2: Coord) -> i64 {
+    let width = (c1.0 - c2.0).abs();
+    let height = (c1.1 - c2.1).abs();
+    (width + 1) * (height + 1)
 }
 
 fn part_2(filename: &str) -> i64 {
@@ -34,26 +55,18 @@ mod tests {
 
     #[test]
     fn part_1_input_example_1() {
-        let answer = part_1("inputs/09_input_example_1.txt", 10);
+        let answer = part_1("inputs/09_input_example_1.txt");
 
         println!("part 1 - example - answer: {:?}", answer);
-        assert_eq!(answer, 40);
-    }
-
-    #[test]
-    fn part_1_input_example_2() {
-        let answer = part_1("inputs/09_input_example_2.txt", 5);
-
-        println!("part 1 - example - answer: {:?}", answer);
-        assert_eq!(answer, 5);
+        assert_eq!(answer, 50);
     }
 
     #[test]
     fn part_1_input() {
-        let answer = part_1("inputs/09_input.txt", 1000);
+        let answer = part_1("inputs/09_input.txt");
 
         println!("part 1 - example - answer: {:?}", answer);
-        assert_eq!(answer, 103488);
+        assert_eq!(answer, 4761736832);
     }
 
     #[test]
