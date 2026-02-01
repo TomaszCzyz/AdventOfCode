67b2803b60abff0ead1b944741af0ef628b6a28c
tczyz <tomczyz97@gmail.com>
2026-02-01 22:56:54 +0100

    add solution for day 11 part 2 - unfinished

--- 2025/src/day_11_reactor.rs
+++ 2025/src/day_11_reactor.rs
@@ -1,19 +1,15 @@
 use std::collections::{HashMap, VecDeque};
-use std::fs;
+use std::{fs, iter};
 
 type Neighbors = Vec<usize>;
 
-// aaa: you hhh
-// you: bbb ccc
-// bbb: ddd eee
-// ccc: ddd eee fff
-// ddd: ggg
-// eee: out
-// fff: out
-// ggg: out
-// hhh: ccc fff iii
-// iii: out
-fn read_input(file_name: &str) -> (Vec<Neighbors>, HashMap<usize, String>) {
+struct Input {
+    neighbors_list: Vec<Neighbors>,
+    name_to_index: HashMap<String, usize>,
+    index_to_name: HashMap<usize, String>,
+}
+
+fn read_input(file_name: &str) -> Input {
     let mut rows_parsed = Vec::<(&str, Vec<&str>)>::new();
     let mut name_to_index = HashMap::new();
     let mut index_to_name = HashMap::new();
@@ -42,18 +38,18 @@ fn read_input(file_name: &str) -> (Vec<Neighbors>, HashMap<usize, String>) {
         }
     }
 
-    (rows, index_to_name)
+    Input {
+        neighbors_list: rows,
+        name_to_index,
+        index_to_name,
+    }
 }
 
-fn part_1(filename: &str) -> usize {
-    let (input, translations) = read_input(filename);
+fn part_1(filename: &str, input_vertx_name: &str) -> usize {
+    let input = read_input(filename);
 
-    let end_vertex = translations.len() - 1;
-    let start_vertex = *translations
-        .iter()
-        .find(|&(_, elem)| elem == "you")
-        .unwrap()
-        .0;
+    let end_vertex = input.name_to_index["out"];
+    let start_vertex = input.name_to_index[input_vertx_name];
 
     let mut paths_count = 0;
     let mut queue = VecDeque::from([start_vertex]);
@@ -64,8 +60,59 @@ fn part_1(filename: &str) -> usize {
             continue;
         }
 
-        for neighbor in &input[vertex] {
+        for neighbor in &input.neighbors_list[vertex] {
             queue.push_back(*neighbor);
+        }
+    }
+
+    paths_count
+}
+
+fn part_2(filename: &str, input_vertx_name: &str) -> i64 {
+    let input = read_input(filename);
+
+    // print_input(&input.neighbors_list, &input.index_to_name);
+
+    let end_vertex = input.name_to_index["out"];
+    let start_vertex = input.name_to_index[input_vertx_name];
+
+    let dac_vertex = input.name_to_index["dac"];
+    let fft_vertex = input.name_to_index["fft"];
+
+    let mut paths_count = 0;
+    let mut queue = VecDeque::from([(start_vertex, vec![start_vertex])]);
+
+    while let Some((vertex, path)) = queue.pop_front() {
+        // println!(
+        //     "visiting vertex: {} - path: {:?}",
+        //     vertex,
+        //     path.iter()
+        //         .map(|&v| &input.index_to_name[&v])
+        //         .collect::<Vec<_>>()
+        // );
+
+        if vertex == end_vertex {
+            if path.contains(&dac_vertex) && path.contains(&fft_vertex) {
+                paths_count += 1;
+            }
+
+            println!(
+                "Path: {:?}",
+                path.iter()
+                    .map(|&v| &input.index_to_name[&v])
+                    .collect::<Vec<_>>()
+            );
+            continue;
+        }
+
+        for neighbor in &input.neighbors_list[vertex] {
+            let new_path = path
+                .iter()
+                .copied()
+                .chain(iter::once(*neighbor))
+                .collect::<Vec<_>>();
+
+            queue.push_back((*neighbor, new_path));
         }
     }
 
@@ -82,19 +129,13 @@ fn print_input(input: &Vec<Neighbors>, translations: &HashMap<usize, String>) {
     }
 }
 
-fn part_2(filename: &str) -> i64 {
-    _ = read_input(filename);
-
-    todo!()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn part_1_input_example_1() {
-        let answer = part_1("inputs/11_input_example_1.txt");
+        let answer = part_1("inputs/11_input_example_1.txt", "you");
 
         println!("part 1 - example - answer: {:?}", answer);
         assert_eq!(answer, 5);
@@ -102,25 +143,44 @@ mod tests {
 
     #[test]
     fn part_1_input() {
-        let answer = part_1("inputs/11_input.txt");
+        let answer = part_1("inputs/11_input.txt", "you");
 
         println!("part 1 - example - answer: {:?}", answer);
         assert_eq!(answer, 640);
     }
 
     #[test]
-    fn part_2_input_example_1() {
-        let answer = part_2("inputs/11_input_example_1.txt");
+    fn part_2_input_example_2() {
+        let answer = part_2("inputs/11_input_example_2.txt", "you");
 
         println!("part 2 - example - answer: {:?}", answer);
-        assert_eq!(answer, 24);
+        assert_eq!(answer, 2);
     }
 
     #[test]
     fn part_2_input() {
-        let answer = part_2("inputs/11_input.txt");
+        let answer = part_2("inputs/11_input.txt", "you");
 
         println!("part 2 - example - answer: {:?}", answer);
-        assert_eq!(answer, 1452422268);
+        assert_eq!(answer, 111);
     }
 }
+// myw: vgt gbv wbp dac
+// dpf: ojt myw nvw
+// kso: ugq xhb axs yky lmw fqi iis sju yma iny nvt dpf ogs zrb lfh coa ivx fef phv hnu
+// luw: kso dgp
+// yzw: che snn luw
+// mem: pmu lcu yzw
+// oxe: ntx mem tkx bkg zxt puu
+// lvm: exf oxe
+// qwu: lvm rfn ifx
+// rcy: msn qwu
+// fwy: anz rcy
+// myd: uer rkw elr jix tgh iup lfl vrj ksy kdn jrm kgz ett cel kzi fwy xwk rmc sgr xop oio tig
+// rhg: myd fkk
+// lno: rhg she reg
+// rvi: lno fpd bgp
+// hpx: xpo zdg tuf rvi
+// axc: hpx gep hza
+// mfc: wvx xjb ndt utx oxi xhs ujk swi ygx ond axc myf qnp jnm bhf jol qqq
+// rjo: mfc
